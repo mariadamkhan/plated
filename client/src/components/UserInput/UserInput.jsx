dc49b556ef792ea459b38a7a6bed073ced56743d
Mari Adamkhan <mariadamkhan@yahoo.ca>
2021-05-08 01:21:54 -0400

    styling uplaod page

--- client/src/components/UserInput/UserInput.jsx
+++ client/src/components/UserInput/UserInput.jsx
@@ -1,21 +1,24 @@
 import React from "react";
+import './UserInput.scss';
 
 const UserInput = ({ label, name, placeholder, onChange, error }) => {
   return (
     <>
-      <label htmlFor="input-label" className="input__label">
-        {label}
-      </label>
-      <input
-        type="text"
-        name={name}
-        className={error ? "input__error" : "input__input"}
-        placeholder={placeholder}
-        onChange={onChange}
-      ></input>
+      <div className="input">
+        <label htmlFor="input-label" className="input__label">
+          {label}
+        </label>
+        <input
+          type="text"
+          name={name}
+          className={error ? "input__error" : "input__input"}
+          placeholder={placeholder}
+          onChange={onChange}
+        ></input>
+      </div>
       {error && (
         <div className="error">
-          <img  alt="error icon" className="error__icon" />
+          <img alt="error icon" className="error__icon" />
           <p className="error__message">{error}</p>
         </div>
       )}
--- client/src/pages/Upload/Upload.jsx
+++ client/src/pages/Upload/Upload.jsx
@@ -64,88 +64,45 @@ export default function Upload() {
     <div className="upload">
       <div className="upload__title-container">
         <img src={Logo} className="upload__logo" alt="Plated Logo" />
-        <h1 className="upload__title"> New Pick</h1>
+        <h1 className="upload__title"> Add New Pick</h1>
       </div>
       <form className="upload__form" name="upload" onSubmit={uploadResto}>
         <div className="upload__left-container">
-          <UserInput
-            label="Name"
-            placeholder="Establishment name"
-            name="name"
-            //   onChange={this.handleChange}
-            //   error={this.state.nameError}
-          />
-          <UserInput
-            label="City"
-            placeholder="City"
-            name="city"
-            //   onChange={this.handleChange}
-            //   error={this.state.cityError}
-          />
+          <UserInput label="Name" placeholder="Name..." name="name" />
+          <UserInput placeholder=" City..." label="City" name="city" />
           <UserInput
             label="Type"
-            placeholder="Establishment type"
+            placeholder="Type/Cuisine..."
             name="cuisine"
-            //   onChange={this.handleChange}
-            //   error={this.state.addressError}
           />
           <>
-            <label htmlFor="input-label" className="input__label">
-              Image
-            </label>
-            <input
-              type="file"
-              name="image"
-              onChange={handleImageAsFile}
-              // className={error ? "input__error" : "input__input"}
-              // onChange={onFileSelect}
-            ></input>
-            {/* {error && (
-                <div className="error">
-                  <img alt="error icon" className="error__icon" />
-                  <p className="error__message">{error}</p>
-                </div>
-              )} */}
+            <div className="input">
+              <label htmlFor="input-label" className="input__label">
+                Image
+              </label>
+              <input
+                type="file"
+                name="image"
+                className="input__input--image"
+                onChange={handleImageAsFile}
+              ></input>
+            </div>
           </>
         </div>
         <div className="upload__right-container">
-          <UserInput
-            label="Phone"
-            placeholder="Phone"
-            name="phone"
-            //   onChange={this.handleChange}
-            //   error={this.state.contactNameError}
-          />
-          <UserInput
-            label="Address"
-            placeholder=" Address"
-            name="address"
-            //   onChange={this.handleChange}
-            //   error={this.state.positionError}
-          />
-          <UserInput
-            label="Hours"
-            placeholder=" Hours"
-            name="hours"
-            //   onChange={this.handleChange}
-            //   error={this.state.phoneError}
-          />
-          <UserInput
-            label="Website"
-            placeholder=" Url"
-            name="url"
-            //   onChange={this.handleChange}
-            //   error={this.state.phoneError}
-          />
+          <UserInput label="Phone" placeholder="Phone..." name="phone" />
+          <UserInput label="Address" placeholder=" Address..." name="address" />
+          <UserInput label="Hours" placeholder=" Hours..." name="hours" />
+          <UserInput label="Url" placeholder=" Url..." name="url" />
         </div>
         <textarea
           type="text"
           className="upload__notes"
           name="note"
-          placeholder="Add a note"
+          placeholder="Add a note..."
         />
         <div className="upload__button">
-          <button className="upload__add" type="submit">
+          <button className="upload__cta" type="submit">
             Add
           </button>
         </div>
